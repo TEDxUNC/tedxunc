bc630d42857124ac712792ab1fc8e33028cd9920
Jon Kaplan <jonkaplan5@gmail.com>
2016-12-22 21:07:16 -0500

    Section wipes added

--- js/_parallax.js
+++ js/_parallax.js
@@ -1,11 +1,42 @@
-// init controller
-var controller = new ScrollMagic.Controller();
+// // init controller
+// //var controller = new ScrollMagic.Controller();
 
-// create a scene
-var scene = new ScrollMagic.Scene({
-                duration: 400,    // the scene should last for a scroll distance of 100px
-                offset: 400        // start this scene after scrolling for 50px
-            })
-            .setPin("#roots") // pins the element for the scene's duration
-            .addTo(controller); // assign the scene to the controller
+// var controller = new ScrollMagic.Controller({
+//     globalSceneOptions: {
+//         triggerHook: 'onLeave'
+//     }
+// });
+
+
+// // create a scene
+// var scene;
             
+// var slides = document.querySelectorAll(".anchor");
+
+// // create scene for every slide
+// for (var i=0; i<slides.length; i++) {
+//     new ScrollMagic.Scene({
+//             triggerElement: slides[i]
+//         })
+//         .setPin(slides[i])
+//         .addIndicators() // add indicators (requires plugin)
+//         .addTo(controller);
+// }
+
+
+var controller = new ScrollMagic.Controller();
+// define movement of panels
+var wipeAnimation = new TimelineMax()
+    .fromTo("#roots", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+    .fromTo("#boroughs",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+    .fromTo("#society", 1, {y: "-200%"}, {y: "0%", ease: Linear.easeNone}) // in from top
+    .fromTo("#humanity", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from bottom
+    // create scene to pin and link animation
+new ScrollMagic.Scene({
+    triggerElement: "body.tedxunc-body",
+    triggerHook: "onLeave",
+    duration: "300%"
+})
+.setPin("body.tedxunc-body")
+.setTween(wipeAnimation)
+.addTo(controller);
