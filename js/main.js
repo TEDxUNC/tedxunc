1fe4fe8dbc02a4dad9b06b8bc57bd81533cefdc6
Jon Kaplan <jonkaplan5@gmail.com>
2017-01-21 16:42:53 -0500

    Many changes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-$(document).ready(function(){
-    $('nav li a').on('click', function(){
-        $('nav li a.selected').removeClass('selected');
-        $(this).addClass('selected');
+var initMap = function(){
+    var loc = {lat: 35.9111, lng: -79.0519};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: loc,
+        scrollwheel: false
     });
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: map
+    });
+}
+
+$(document).ready(function(){
+    //initMap();
 
     var top1 = $('#home').offset().top;
     var top2 = $('#speakers').offset().top;
     var top3 = $('#faq').offset().top;
     var top4 = $('#sponsors').offset().top;
     var top5 = $('#contact').offset().top;
-
-    console.log(top1, top2, top3, top4, top5);
 
     $(document).scroll(function() {
         if ($(document).scrollTop() >= top1 && $(document).scrollTop() < top2) {
@@ -41,4 +49,26 @@ $(document).ready(function(){
         }
     });
 
+    var accordion = $(".accordion");
+
+    for (var i = 0; i < accordion.length; i++) {
+        accordion[i].onclick = function(){
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+            if (panel.style.maxHeight){
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + 'px';
+            } 
+        }
+    }
 });
--- js/parallax.js
+++ js/parallax.js
@@ -1,43 +1,11 @@
 var parallax = function(){
 
     var controller = new ScrollMagic.Controller();
-    // define movement of panels
-    // var wipeAnimation = new TimelineMax()
-    //     .fromTo("#roots", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-    //     .fromTo("#boroughs",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-    //     .fromTo("#society", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
-    //     .fromTo("#humanity", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}) // in from bottom
-    //     .fromTo("#speakers", 1, {x: "-100%", y: "100%"}, {x: "0%", y: "0%", ease: Linear.easeNone})
-    //     .fromTo("#committee", 1, {x: "100%", y: "100%"}, {x: "0%", y: "0%", ease: Linear.easeNone})
-    //     .fromTo("#faq", 1, {x: "-100%", y: "-100%"}, {x: "0%", y: "0%", ease: Linear.easeNone})
-    //     .fromTo("#contact", 1, {x: "100%", y: "-100%"}, {x: "0%", y: "0%", ease: Linear.easeNone});
-    // new ScrollMagic.Scene({
-    //     triggerElement: "body.tedxunc-body",
-    //     triggerHook: "onLeave",
-    //     duration: "300%"
-    // })
-    // .setPin("body.tedxunc-body")
-    // .setTween(wipeAnimation)
-    // .addTo(controller);
-
-    // get all slides
-    var slides = document.querySelectorAll(".anchor");
-
-    // create scene for every slide
-    for (var i=0; i<slides.length; i++) {
-        new ScrollMagic.Scene({
-                triggerElement: slides[i],
-                triggerHook: 0
-            })
-            .setPin(slides[i])
-            .addTo(controller);
-    }
-
 
     // change behaviour of controller to animate scroll instead of jump
-	// controller.scrollTo(function (newpos) {
-	// 	TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
-	// });
+	controller.scrollTo(function (newpos) {
+		TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+	});
 
 	//  bind scroll to anchor links
 	$(document).on("click", "a[href^='#']", function (e) {
